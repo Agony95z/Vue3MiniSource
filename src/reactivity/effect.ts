14829d2e7eb8b693e446bac5a0e513d165a1fc41
张卫海 <zhangweihai@zichan360.com>
2022-02-12 15:01:27 +0800

    effect 补充功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,15 +1,35 @@
+import { extend } from "../shared";
+
 class ReactiveEffect {
   private _fn: any
-  constructor(fn) {
+  deps = []
+  active = true
+  onStop?: () => void
+  constructor(fn, public scheduler?) {
     this._fn = fn;
   }
   run() {
     activeEffect = this
     return this._fn()
   }
+  // 清除effect副作用 比如 更改响应式数据 不去触发effect副作用 stop(runner) runner为函数副作用 也就是effect的入参 -- 回调函数
+  stop() {
+    if (this.active) {
+      cleanupEffect(this)
+      if (this.onStop) {
+        this.onStop()
+      }
+      this.active = false
+    }
+  }
 }
 
-
+// 清除deps 中的 effect
+function cleanupEffect (effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  });
+}
 const targetMap = new Map()
 export function track (target, key) {
   // target -> key -> dep
@@ -23,21 +43,36 @@ export function track (target, key) {
     dep = new Set()
     depsMap.set(key, dep)
   }
-  dep.add(activeEffect)
+  if (!activeEffect) return; // 针对单纯的触发reactive中的get--track而没有触发effect副作用 也就不存在activeEffect
+  dep.add(activeEffect) // 收集effect副作用
+  activeEffect.deps.push(dep) // 收集dep
 }
 
 export function trigger (target, key) {
   let depsMap = targetMap.get(target)
   let dep = depsMap.get(key)
   for (const effect of dep) {
-    effect.run()
+    if (effect.scheduler) { // effect 传入第二个参数执行第二个参数
+      effect.scheduler()
+    } else { // 否则执行第一个参数
+      effect.run()
+    }
   }
 }
 
 let activeEffect;
-export function effect (fn) {
+export function effect (fn, options:any = {}) {
+  // const scheduler = options.scheduler;
   // fn
-  const _effect = new ReactiveEffect(fn)
+  const _effect = new ReactiveEffect(fn, options.scheduler)
+  // _effect.onStop = options.onStop
+  extend(_effect, options)
   _effect.run()
-  return _effect.run.bind(_effect) // 绑定run方法里的this指针
+  const runner: any = _effect.run.bind(_effect) // 绑定run方法里的this指针
+  runner.effect = _effect
+  return runner
+}
+
+export function stop (runner) {
+  runner.effect.stop()
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,4 +1,4 @@
-import { effect } from "../effect";
+import { effect, stop } from "../effect";
 import { reactive } from "../reactive";
 describe('effect', () => {
   it('happy path', () => {
@@ -25,5 +25,56 @@ describe('effect', () => {
     let r = runner();
     expect(r).toBe("foo");
     expect(foo).toBe(12)
+  });
+  it("scheduler", () => {
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner; // 调用scheduler 把() => {dummy = obj.foo;}复制给run 后面执行run 则执行() => {dummy = obj.foo;}()
+    });
+    const obj = reactive({ foo: 1 });
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { scheduler } // 函数副作用传入scheduler 当触发响应式数据set--trigger的时候 才会调用scheduler
+    );
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+    // should be called on first trigger
+    obj.foo++;
+    expect(scheduler).toHaveBeenCalledTimes(1); // 调用schedule 而不是调用() => {dummy = obj.foo;}
+    // // should not run yet
+    expect(dummy).toBe(1);
+    // // manually run
+    run(); // 执行() => {dummy = obj.foo;}
+    // // should have run
+    expect(dummy).toBe(2);
+  });
+  it("stop", () => {
+    let dummy;
+    const obj = reactive({ prop: 1 });
+    const runner = effect(() => {
+      dummy = obj.prop;
+    });
+    obj.prop = 2;
+    expect(dummy).toBe(2);
+    stop(runner);
+    // obj.prop = 3
+    expect(dummy).toBe(2);
+    obj.prop++;
+
+    // stopped effect should still be manually callable
+    runner();
+    expect(dummy).toBe(3);
+  });
+  it("events: onStop", () => { // 调用stop之后的一个回调函数
+    const onStop = jest.fn();
+    const runner = effect(() => {}, {
+      onStop,
+    });
+
+    stop(runner);
+    expect(onStop).toHaveBeenCalled();
   });
 });
